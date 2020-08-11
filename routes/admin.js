64107859b45f5dcdec7b6ebc87b94b348bd3a6c0
Franco Tagliero <ftagliero@gmail.com>
2020-08-11 19:24:14 -0300

    added filtering path, serving html and files statically

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
+const path = require("path");
+
 const express = require("express");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Submit!</button>'
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   const obj = JSON.parse(JSON.stringify(req.body));
   console.log(obj);
   res.redirect("/");
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,11 @@
+const path = require("path");
+const rootDir = require("../util/path");
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from express!</h1>");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
