87cfe8c1b02638fd0bab141afd4a2402f46b8f34
Franco Tagliero <ftagliero@gmail.com>
2020-08-12 16:18:13 -0300

    added some pug templates

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require("path");
 const rootDir = require("../util/path");
 
 const express = require("express");
+const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
