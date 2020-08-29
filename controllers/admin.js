e785b70b5ab445c28f927bb45a2ca99198ef983a
Franco Tagliero <ftagliero@gmail.com>
2020-08-29 18:15:39 -0300

    Able to edit product data .

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, _next) => {
+exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
@@ -8,17 +8,34 @@ exports.getAddProduct = (req, res, _next) => {
     });
 };
 
-exports.postAddProduct = (req, res, _next) => {
+exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect("/");
 };
 
-exports.getEditProduct = (req, res, _next) => {
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(
+        prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedDesc,
+        updatedPrice
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
+};
+
+exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
     if (!editMode) {
         return res.redirect('/');
@@ -38,7 +55,7 @@ exports.getEditProduct = (req, res, _next) => {
 
 };
 
-exports.getProducts = (_req, res, _next) => {
+exports.getProducts = (_req, res, next) => {
     Product.fetchAll(products => {
         res.render('admin/products', {
             prods: products,
